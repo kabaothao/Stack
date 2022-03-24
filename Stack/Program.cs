435480a56c0a696ca686aaf1de4e65f42eaf4fbd
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-24 09:26:20 -0500

    Add project files.

---
+++ Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+/*
+ Stack - how it works is it's a last in first out collection
+ */
