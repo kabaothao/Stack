6bdfcc2f18ea36423624394d326ab3e854587276
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-24 10:09:56 -0500

    updated codes

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -19,6 +19,23 @@ namespace MyApp // Note: actual namespace depends on the project name.
             stack.Push(2);
             stack.Push(3);
 
+            //peek at an item. return the object at the top of the Stack without removing it. 
+            Console.WriteLine("Peek 1 : {0}", stack.Peek());
+
+            //remove an item. is used to remove and returns the object at the top of the Stack. This method is similar to the Peek method, but Peek does not modify the Stack.
+            Console.WriteLine("Pop 1 : {0}", stack.Pop());
+
+            Console.WriteLine("Contain 1 : {0}", stack.Contains(1));
+
+            object[] numArray2 = stack.ToArray();
+
+            Console.WriteLine(String.Join(", ", numArray2));
+
+            stack.Clear();
+
+
+
+
             foreach (object o in stack)
             {
                 Console.WriteLine($"Stack : {o}");
